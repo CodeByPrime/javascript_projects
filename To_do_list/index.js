2cea72775c0ff29861616830a5ab3b5249321b10
CodeByPrimes <mrcody497@gmail.com>
2025-08-28 13:21:51 +0500

    a to-do list project

---
+++ To_do_list/index.js
@@ -0,0 +1,30 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('List-container');
+const addBtn = document.querySelector('button');
+
+addBtn.addEventListener('click', addTask);
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert('You must write something!');
+    } else {
+        const li = document.createElement('li');
+        li.innerText = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+
+    }
+    inputBox.value = '';
+
+}
+
+listContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false);
